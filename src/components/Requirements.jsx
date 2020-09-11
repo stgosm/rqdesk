482bbaddbbb5c42f07f7b79426bdc34128a561fb
StgoSm <santiago.serrano.marquez@hotmail.com>
2020-09-10 20:17:18 -0500

    Haciendo componente Requirements funcional y agregando solicitud de un Fake REST API

--- src/components/Requirements.jsx
+++ src/components/Requirements.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
 import Request from './Request'
-import rqdata from '../assets/requerimientos.json';
+/*import rqdata from '../assets/requerimientos.json';*/
 import Grid from '@material-ui/core/Grid';
 
-class Requirements extends React.Component {
-  state = {
-    requirements: []
-  }
-
-  componentDidMount(){
-    this.setState({ requirements: rqdata});
-  }
 
 
-  render(){
-    return(
-        <>
-          {
-            this.state.requirements.map(rq => 
-              <Grid item xs={12} sm={6} md={6} lg={3} key={ rq.id }>
-                <Request {...rq} />
-              </Grid>
-            )
-          }
-        </>
-    )
-  }
+
+function Requirements() {
+
+  const [requeriments, setRequirements] = React.useState([])
+
+  React.useEffect(() => {
+    async function getRequirements() {
+      try {
+        const data = await fetch('http://localhost:3000/requiriments')
+        const dataRequiriments = await data.json()
+
+        setRequirements(dataRequiriments)
+        
+      } catch(e) {
+        console.error(e);
+      };
+    }
+
+    getRequirements();
+  }, []);
+
+  return(
+    <>
+      {
+        requeriments.map(rq => 
+          <Grid item xs={12} sm={6} md={6} lg={3} key={ rq.id }>
+            <Request {...rq} />
+          </Grid>
+        )
+      }
+    </>
+  )
 }
 
 export default Requirements;
