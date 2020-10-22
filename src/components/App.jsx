ed64fcc78eb42a5713ce99e59fcbedb5b1bec555
stgosm <santiago.serrano.marquez@hotmail.com>
2020-10-21 20:38:08 -0500

    Minor fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Requirements from './Requirements';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
