214709039ecff9b68c56cd3322fd9fc76489e5e0
stgosm <santiago.serrano.marquez@hotmail.com>
2020-10-21 20:41:53 -0500

    Adding query to find Requirements and minor fixes.

--- src/components/Requirements.jsx
+++ src/components/Requirements.jsx
@@ -1,36 +1,77 @@
 import React from 'react';
-import Request from './Request'
-/*import rqdata from '../assets/requerimientos.json';*/
+import Request from './Request';
 import Grid from '@material-ui/core/Grid';
 
+import { gql, useQuery, useLazyQuery } from '@apollo/client';
 
+const ALL_REQUIREMENTS = gql`
+query {
+  requirements {
+    _id
+    rqName
+    rqArea {
+      areaName
+    }
+    rqDescription
+  }
+}
+`
+
+const FIND_REQUIREMENT = gql`
+  query findRequirementById($requirementToSearch: String!) {
+    findRequirement(_id: $requirementToSearch){
+      rqName
+      rqDescription
+      rqArea
+      rqStatus
+      rqTags
+      rqUploaded
+      rqUser
+    }
+  }
+`
 
 
 function Requirements() {
 
-  const [requeriments, setRequirements] = React.useState([])
+  const [getRequirement, resultRequirement] = useLazyQuery(FIND_REQUIREMENT);
+  const [requirement, setRequirement] = React.useState([]);
+  const showRequirement = (_id) => {    
+    getRequirement({ variables: { requirementToSearch: _id } })  
+  }
+  React.useEffect(() => {    
+      if (resultRequirement.data) {      
+        setRequirement(resultRequirement.data.findRequirement)    
+    }  
+  }, [resultRequirement])
 
-  React.useEffect(() => {
-    async function getRequirements() {
-      try {
-        const data = await fetch('http://localhost:3000/requiriments')
-        const dataRequirements = await data.json()
+  const result = useQuery(ALL_REQUIREMENTS);
 
-        setRequirements(dataRequirements)
+  // React.useEffect(() => {
+  //   async function getRequirements() {
+  //     try {
+  //       const data = await fetch('http://localhost:3000/requiriments')
+  //       const dataRequirements = await data.json()
+
+  //       setRequirements(dataRequirements)
         
-      } catch(e) {
-        console.error(e);
-      };
-    }
+  //     } catch(e) {
+  //       console.error(e);
+  //     };
+  //   }
 
-    getRequirements();
-  }, []);
+  //   getRequirements();
+  // }, []);
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return(
     <>
       {
-        requeriments.map(rq => 
-          <Grid item xs={12} sm={6} md={6} lg={3} key={ rq.id }>
+        result.data.requirements.map(rq => 
+          <Grid item xs={12} sm={6} md={6} lg={3} key={ rq._id }>
             <Request {...rq} />
           </Grid>
         )
